73bd2448647ac57fd2f65dff7ffe8f50a4b8e3b5
jonuigb1 <jonuigb1@my.westga.edu>
2018-02-01 16:02:00 -0500

    Created a Sort class and a TestSort class Created a project called OnuigboProject2Data

---
+++ src/edu/westga/cs3151/project2/Sort.java
@@ -0,0 +1,36 @@
+package edu.westga.cs3151.project2;
+
+import java.util.Random;
+
+public class Sort {
+	public static <T> void swap(T[] data, int i, int j) {
+		T tmp = data[i];
+		data[i] = data[j];
+		data[j] = tmp;
+	}
+	public static <T> void shuffle(T[] data) {
+		Random rnd = new Random();
+		
+		for(int n = data.length-1; n > 0;n--) {
+			int m = rnd.nextInt(n + 1);
+			if(m != n) {
+				swap(data , n , m);
+			}
+		}
+	}
+	
+	public static <T extends Comparable<T>> void insertionSort(T[] data){
+		for(int i = 1 ;i < data.length ; i++) {
+			T x = data[i];
+			int j= i - 1;
+			while(j >= 0 && (data[j].compareTo(x) > 0)) {
+				swap( data ,j + 1, j);
+				j--;
+			}
+			data[j + 1] = x;
+		}
+		return;
+	}
+	
+
+}
---
+++ src/edu/westga/cs3151/project2/TestSort.java
@@ -0,0 +1,33 @@
+package edu.westga.cs3151.project2;
+
+public class TestSort {
+	
+
+	public static void main(String[] args) {
+		
+		int max = 20;
+		Integer[] array = new Integer[max];
+		
+		for(int i=0;i<max;i++) {
+			array[i] = i;
+		}
+		
+		for(int i=0;i<max;i++) {
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+		Sort.shuffle(array);
+		for(int i=0;i<max;i++) {
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+		long start = System.currentTimeMillis();
+		Sort.insertionSort(array);
+		long end = System.currentTimeMillis();
+		long elapsed = end - start;
+		for(int i=0;i<max;i++) {
+			System.out.print(array[i]+" ");
+		}
+	}
+
+}
